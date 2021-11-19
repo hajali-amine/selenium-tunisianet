43e7a138916e91ff1c764a15ffaa7516e0c4cf0c
unknown <aminehajali@insat.u-carthage.tn>
2021-11-19 20:12:19 +0100

    clean code

---
+++ readme.md
@@ -0,0 +1,11 @@
+# TunisiaNet meets Selenium
+This repository contains a small Selenium script that goes through TunisiaNet's website.
+
+## Scenario
+1. __Authenticate:__ sign up with a new account, logout and login. 
+1. __Choose a product:__ search using the searchbar and find product.
+1. __Order:__ add to cart, check cart and finalize.
+
+#### Go to *src/test/java/TunisiaNetTests.java* to check the code :)
+
+Click [here](https://drive.google.com/file/d/1bl2KqE2nuWUC0zvLWpjQmLu3OrBlph8H/view?usp=sharing) to watch the demo.
--- src/test/java/TunisiaNetTests.java
+++ src/test/java/TunisiaNetTests.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import models.Account;
 import org.apache.commons.lang.RandomStringUtils;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -33,6 +34,12 @@ public class TunisiaNetTests {
 
     @Test
     public void buyMacTestCase() throws InterruptedException {
+        // -------------- Test Scenario --------------
+        // 1 - Create an account, logout and login
+        // 2 - Search for Mac
+        // 3 - Add it to cart and order
+        // -------------------------------------------
+
         driver.get("https://www.tunisianet.com.tn/");
 
         String pattern = "dd/MM/yyyy";
@@ -47,22 +54,28 @@ public class TunisiaNetTests {
                 new Date(new Date().getTime() - TimeUnit.DAYS.toMillis(1) * 365 * 18) // 18 years ago
         );
 
+        // Click on the user icon
         Thread.sleep(1500); // TODO: Find a better way to wait for an element to become clickable
         WebElement userInfoDropdown = driver.findElement(By.cssSelector("#_desktop_user_info > div > div > svg"));
         userInfoDropdown.click();
 
+        // Click on the "Connexion" button
         Thread.sleep(1500);
         WebElement signinButton = driver.findElement(By.cssSelector(".user-down > li > a > span"));
         signinButton.click();
 
+        // Click to create an account
         Thread.sleep(1500);
         WebElement createAccountButton = driver.findElement(By.className("no-account"));
+        Assert.assertEquals("Pas de compte ? Créez-en un", createAccountButton.findElement(By.cssSelector("*")).getText());
         createAccountButton.click();
 
+        // Choose the male option
         Thread.sleep(1500);
         List<WebElement> genderOptions = driver.findElements(By.className("custom-radio"));
         genderOptions.get(0).click();
 
+        //Fill the rest of the form with the random account information
         Thread.sleep(1500);
         List<WebElement> signupFormFields = driver.findElements(By.cssSelector("input.form-control"));
         // Element with index 0 is the searchbar
@@ -72,59 +85,72 @@ public class TunisiaNetTests {
         signupFormFields.get(4).sendKeys(userAccount.password);
         signupFormFields.get(5).sendKeys(dateFormatter.format(userAccount.birthday));
 
-        js.executeScript("window.scrollBy(0,250)", "");
+        js.executeScript("window.scrollBy(0,250)", ""); // Scroll down
 
+        // Click to sign-up
         Thread.sleep(1500);
         WebElement signupButton = driver.findElement(By.className("form-control-submit"));
         signupButton.click();
 
+        // Click on the user icon
         Thread.sleep(1500);
         userInfoDropdown = driver.findElement(By.cssSelector("#_desktop_user_info > div > div > svg"));
         userInfoDropdown.click();
 
+        // Click on the logout button
         Thread.sleep(1500);
         WebElement signoutButton = driver.findElement(By.className("logout"));
         signoutButton.click();
 
+        // As usual, click on the user icon
         Thread.sleep(1500);
         userInfoDropdown = driver.findElement(By.cssSelector("#_desktop_user_info > div > div > svg"));
         userInfoDropdown.click();
 
+        // Click on "Connexion"
         Thread.sleep(1500);
         signinButton = driver.findElement(By.cssSelector(".user-down > li > a > span"));
         signinButton.click();
 
+        // Fill the email field
         Thread.sleep(1500);
         WebElement emailTextField = driver.findElement(By.cssSelector(".form-group > div > input"));
         emailTextField.sendKeys(userAccount.email);
 
+        // Fill the password field
         Thread.sleep(1500);
         WebElement pwdTextField = driver.findElement(By.cssSelector(".form-group > div > div > input"));
         pwdTextField.sendKeys(userAccount.password);
 
+        // Click on sign in
         Thread.sleep(1500);
         WebElement submitButton = driver.findElement(By.id("submit-login"));
         submitButton.click();
 
+        // Search for the desired Mac
         Thread.sleep(1500);
         WebElement searchBar = driver.findElement(By.className("search_query"));
         searchBar.sendKeys("PC portable MacBook M1 13.3");
 
+        // Click to search
         Thread.sleep(1500);
         WebElement searchButton = driver.findElement(By.cssSelector("#sp-btn-search > button"));
         searchButton.click();
 
+        // Click on the first product
         Thread.sleep(1500);
         List<WebElement> productsTitle = driver.findElements(By.className("product-title"));
         productsTitle.get(0).click();
 
+        // Add product to cart
         Thread.sleep(1500);
         WebElement addToCartButton = driver.findElement(By.className("add-to-cart"));
         addToCartButton.click();
 
+        // Click to order
         Thread.sleep(1500);
-        WebElement commandButton = driver.findElement(By.cssSelector("a.btn-block"));
-        commandButton.click();
+        WebElement orderButton = driver.findElement(By.cssSelector("a.btn-block"));
+        orderButton.click();
     }
 
     @After
